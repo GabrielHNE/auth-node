632c6648d1514cb2a68f988667386e2b39e30cb5
Gabriel H <gabrielhnespindola@gmail.com>
2020-06-11 18:05:16 -0300

    Setup migrations for users

--- src/server.js
+++ src/server.js
@@ -1,9 +1,28 @@
 const express = require('express');
 
+const authRoute = require('./controllers/AuthController');
 
 const PORT = 3030;
 
 const app = express();
+
+
+app.use(express.json());
+app.use('/auth', authRoute);
+
+//handling page not found
+app.use((req, res, next) => {
+    const err = new Error("Page not Found 404");
+    err.status = 404;
+    next(err);
+});
+
+
+//handling errors
+app.use((err, req, res, next) => {
+    res.status(err.status || 500);
+    res.json({ error: err.message, err});
+});
 
 app.get('/', (req, res, next) => {
     res.send('HI');
