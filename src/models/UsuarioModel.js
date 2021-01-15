675fcc4e739f8c976c052cca3feb7b680900fbab
GabrielHNE <gabrielhnespindola@gmail.com>
2021-01-15 10:14:55 -0300

    Users model created

--- src/models/UsuarioModel.js
+++ src/models/UsuarioModel.js
@@ -1,11 +1,15 @@
 const knex = require('../database/connection');
+const bcrypt = require('bcrypt');
 
 class Usuario {
     
     constructor(usuario){
+        this.id = usuario.id;
         this.name = usuario.name;
         this.email  = usuario.email;
         this.hash = usuario.hash;
+
+        //validUserValues();
     }
 
     async addUser(){
@@ -17,11 +21,25 @@ class Usuario {
             await knex('users').insert({ 
                 name: this.name,
                 email: this.email,
-                password: this.password
+                password: this.hash
             })
         }catch(e){
             throw new Error(e);
         }
+    }
+
+    async addPasswor(password){
+        //functions that check if password is correct
+
+        this.hash = await Usuario.generateHash(password);
+    }
+    
+    async delete(){
+        return await knex('users').where({ id: this.id }).del();
+    }
+
+    static async listUsers(){
+        return knex('users').select("*");
     }
 
     static async getByEmail(email){
@@ -33,10 +51,14 @@ class Usuario {
     }
 
     static async getById(id){
-        const usuario = await knex('users').where('id', id).first();
+        const usuario = await knex('users').where({id:id}).first();
 
         if(!usuario) return null;
 
         return new Usuario(usuario);
+    }
+
+    static async generateHash(password){
+        return await bcrypt.hash(password, 12);
     }
 }
